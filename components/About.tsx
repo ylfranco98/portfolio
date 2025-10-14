19621dd91c8bbfdb84567101d8d42210b16effae
Yoana <ylfranco98@gmail.com>
2025-10-13 22:42:27 -0400

    test

--- components/About.tsx
+++ components/About.tsx
@@ -4,6 +4,7 @@ const About: React.FC = () => {
   return (
     <section className="relative flex items-center justify-between mt-[50px] z-[1] pl-[5%] pr-[5%]">
       {/* Add your content here */}
+      Test
     </section>
   );
 };
