c0bb576f0a5a1f03138bac37286fd7f73e35e9ef
Yoana <ylfranco98@gmail.com>
2025-11-24 21:00:09 -0500

    implement experience section

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth scroll-pt-[160px]">
       <body>{children}</body>
     </html>
   );
